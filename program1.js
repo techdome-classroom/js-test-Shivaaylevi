9121e8652631474f6dc5eb559191cc404ac38c7d
shivam mishra <shivammishra1806@gmail.com>
2024-05-09 12:43:50 +0530

    first answer

--- program1.js
+++ program1.js
@@ -1,10 +1,26 @@
 const getTotalIsles = function (grid) {
 
+  let counter = 0;
+  for (let i in grid) {
+    for (let j in grid[i]) {
+      if (grid[i][j] === "L") {
+        counter++;
+        check(parseInt(i), parseInt(j), grid)
+      }
+    }
+  }
+  return counter;
+};
 
-    // write your code here
+const check = (row, col, grid) => {
+  if (grid[row] === undefined || grid[row][col] === undefined || grid[row][col] === "W") {
+    return;
+  }
+  grid[row][col] = "W";
+  check(row + 1, col, grid)
+  check(row - 1, col, grid)
+  check(row, col + 1, grid)
+  check(row, col - 1, grid)
+};
 
-  };
-  
-  module.exports = getTotalIsles;
-
-
+module.exports = getTotalIsles;
